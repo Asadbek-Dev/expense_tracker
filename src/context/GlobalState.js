1f6bef376ecd987edbdd23642b019f00ed3baf6a
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-08-22 00:30:52 +0500

    worked GlobalState

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,9 +1,9 @@
 import React,{ createContext, useReducer } from 'react';
-
+import AppReducer from './AppReducer'
 
 
 const initialState={
-    transactions=[
+    transactions : [
         {id:1,text:'Flower',amount:-20},
         {id:2,text:'Salary',amount:300},
         {id:3,text:'Book',amount:-10},
