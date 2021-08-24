5cf2e551623f83f5c34f199de4eadcd2830ca3ac
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-08-24 13:09:00 +0500

    changed style

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -3,14 +3,14 @@ import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
     const {transactions}=useContext(GlobalContext);
-    console.log(transactions);
+
 
     return (
         <div>
             <h3>History</h3>
             <ul className="list">
                 {transactions.map(transaction=>( 
-                    <li key={transaction.id} className='minus'>
+                    <li key={transaction.id} className={transaction.amount < 0 ? 'minus':'plus'}>
                         {transaction.text}<span>{transaction.amount<0?'-':'+'}${Math.abs(transaction.amount)}</span><button className='delete-btn'>x</button>
                     </li>
                 ))}
