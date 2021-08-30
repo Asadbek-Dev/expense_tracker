bb2ec7b55c2ebe9992951c043d4df739e2d339ff
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-08-30 12:39:00 +0500

    added addTransactions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
 
+    const {addTransaction} = useContext(GlobalContext);
+
+    const onSubmit = e =>{
+        e.preventDefault();
+
+        const newTransaction={
+            id:Math.floor(Math.random() * 10000000),
+            text,
+            amount:+amount
+        }
+        addTransaction(newTransaction);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder='Enter text...' />
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -26,11 +26,18 @@ export const GlobalProvider = ({children})=>{
             payload:id
         });
     }
+    function addTransaction(transaction){
+        dispatch({
+            type:'ADD_TRANSACTION',
+            payload:transaction
+        });
+    }
 
     return(
         <GlobalContext.Provider value={{
             transactions:state.transactions,
-            deleteTransaction
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
